176e26d645e74c11f1eceb1997ca197635eb7df5
Jerome Onadja <jeromeonadja28@gmail.com>
2023-01-08 14:49:13 +0000

    enhance: update install.sh file

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+# Clone kadi repository
+git clone https://github.com/user/kadi.git
+
 # Create .kadi directory
 mkdir ~/.kadi
 
@@ -7,3 +11,6 @@ mv kadi/kadi.sh ~/.kadi/kadi.sh
 
 # Add alias to .profile
 echo "alias kadi='~/.kadi/kadi.sh'" >>~/.profile
+
+# source the .profile
+source ~/.profile
